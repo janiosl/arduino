eb0c41ee82f2e6c3fde65ce1e385d29320ec0a60
janiosl <56620033+janiosl@users.noreply.github.com>
2019-12-01 12:36:43 -0300

    Update sketch_sensor_plantas.ino
    Inclusão de comentários complementares sobre o valor de referência da humidade.

--- sketch_sensor_plantas.ino
+++ sketch_sensor_plantas.ino
@@ -1,6 +1,18 @@
+/*
+Projeto Bot Green: Sensor de humidade para plantas
+As fotos do projeto estão na página do Instagram @python.ds
+*/
+
+//Led vermelho conectado em GND e Pino digital 13
 int pinLed = 13;
+//Led verde conectado na protoboard
 int pinLed_green = 7;
+//Fio conectado à porta analógica 0 à protoboard para conexão com um dos pregos do sensor
+//Entre este fio e o fio ligado ao prego há um resisotr de 10ohms
+//No outro do resistor é colocado outro fio conectado ao GND do Arduino
 int pinSensor = 0;
+
+//Variável para registrar o valor lido pelo sensor
 int leituraSensor;
 
 void setup() {
@@ -14,10 +26,17 @@ void loop() {
   leituraSensor = analogRead(pinSensor);
   delay(1000);
 
-  /*Se o valor for menor que 800, significa que está
+  /*Se o valor for menor que 250, significa que está
   faltando água. O led de alerta é ligado e a mensagem
-  exibida no monitor*/
+  exibida no monitor
+  O valor de 250 no exemplo é arbitrário, baseado nos testes que fiz
+  numa plana esṕecífica. O ideal é rodar conectado ao monitor serial e
+  verificar os valores adequados para avaliar a humidade de acordo com
+  a terra usada e a demanda por água da planta
+  */
   if (leituraSensor < 250) {
+    //Como aprimoramente, esse limite poderia ser substituído por uma variável int no início
+    //do código, para não ser necessário alterar diretamente na função.
     digitalWrite(pinLed_green, LOW);
     digitalWrite(pinLed, HIGH);
     Serial.println("Me alimente!");
